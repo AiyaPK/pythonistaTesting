0cef83cd73c6140c6db97dc33ff0d8d80af563fb
AiyaPK <36603773+AiyaPK@users.noreply.github.com>
2019-08-07 18:36:01 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
 print("hello")
+print("This line has been added via browser in gh")
+print('This is an additional line with single quotes added in gh')
+def test():
+  print("this is a function test made in gh")
+test()
