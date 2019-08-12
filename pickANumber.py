ce09e2af7ff7f53020283e97560cd54900f395e2
aiyapk <>
2019-08-12 03:51:22 +0000

    testing

--- pickANumber.py
+++ pickANumber.py
@@ -1,33 +1,33 @@
 #for pythonista
 
 import random
-#import ui
-#view=ui.load_view('pickANumber')
-#view.present('fullscreen')
+import ui
+view=ui.load_view('pickANumber')
+view.present('fullscreen')
 
-#minNum = view['min']
-#maxNum = view['max']
+def rank(sender):
+	pass
 
-ValDict={}
-for i in range(4):
-  if view[f"num+{i}"]:
-    ValDict[view[f"name+{i}"]]=view[f"num+{i}"]
-print(ValDict)
-
-
-
-nameValDict = {"Tammy":3,"James":8,"Peter":1}
-
+minNum = int(view['minNum'].text)
+maxNum = int(view['maxNum'].text)
 randPick = random.randint(minNum,maxNum)
+print(randPick)
 
-"""
+valDict={}
+for i in range(6):
+	iName='name_'+str(i)
+	iNum='num_'+str(i)
+	if view[iNum].text:
+		valDict[view[iName].text]=int(view[iNum].text)
+
 diffNamesDict={}
-for nameVal in nameValDict:
-  valDiff = abs(nameValDict[nameVal]-randPick)
-  diffNamesDict[nameVal]=valDiff
+for nameVal in valDict:
+	valDiff = abs(valDict[nameVal]-randPick)
+	diffNamesDict[nameVal]=valDiff
 
 for key,value in sorted(diffNamesDict.items(),key=lambda x:x[1]):
-  print(f"{key}: \u0394{value}")
-"""
+	print(f"{key}: \u0394{value}")
+
 
 #print(randPick)
+
