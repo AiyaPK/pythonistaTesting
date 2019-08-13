b673c42af0bb6571c67936da8a579c1f910714cc
AiyaPK <36603773+AiyaPK@users.noreply.github.com>
2019-08-13 16:31:18 -0700

    Update pickANumber.py
    adding ui class

--- pickANumber.py
+++ pickANumber.py
@@ -2,32 +2,45 @@
 
 import random
 import ui
-view=ui.load_view('pickANumber')
-view.present('fullscreen')
 
-def rank(sender):
-	pass
+class PickANum(ui.view):
+  v_main = None
+  v_results = None
 
-minNum = int(view['minNum'].text)
-maxNum = int(view['maxNum'].text)
-randPick = random.randint(minNum,maxNum)
-print(randPick)
+def __init__(self):
 
-valDict={}
-for i in range(6):
-	iName='name_'+str(i)
-	iNum='num_'+str(i)
-	if view[iNum].text:
-		valDict[view[iName].text]=int(view[iNum].text)
+  self.v_main=ui.load_view('pickANumber')
+  self.v_main.present('fullscreen')
 
-diffNamesDict={}
-for nameVal in valDict:
-	valDiff = abs(valDict[nameVal]-randPick)
-	diffNamesDict[nameVal]=valDiff
+def rank(self, sender):
 
-for key,value in sorted(diffNamesDict.items(),key=lambda x:x[1]):
-	print(f"{key}: \u0394{value}")
+  def closeme(sender):
+    self.v_results.close()
 
+  minNum = int(self.v_main['minNum'].text)
+  maxNum = int(self.v_main['maxNum'].text)
+  randPick = random.randint(minNum,maxNum)
+  valDict={}
 
-#print(randPick)
+  for i in range(6):
+    iName='name_'+str(i)
+    iNum='num_'+str(i)
+    if self.v_main[iNum].text:
+      valDict[self.v_main[iName].text]=int(self.v_main[iNum].text)
+
+  diffNamesDict={}
+  for nameVal in valDict:
+    valDiff = abs(valDict[nameVal]-randPick)
+    diffNamesDict[nameVal]=valDiff
+
+  resultList = [];
+
+  for key,value in sorted(diffNamesDict.items(),key=lambda x:x[1]):
+    nameResult = f"{key}: \u0394{value}"
+    resultList.append(nameResult)
+  
+  self.v_results = ui.load_view('pANpopout')
+  self.v_results.present('popover')
+  resultViewBox = self.v_results['results']
+  resultViewBox.data_source=resultList
 
