ed56e97a454e9945c4365c9cb7cae75495b7a363
AiyaPK <36603773+AiyaPK@users.noreply.github.com>
2019-08-13 16:36:51 -0700

    Update pickANumber.py

--- pickANumber.py
+++ pickANumber.py
@@ -3,44 +3,45 @@
 import random
 import ui
 
-class PickANum(ui.view):
+class PickANum(ui.View):
   v_main = None
   v_results = None
 
-def __init__(self):
+  def __init__(self):
 
-  self.v_main=ui.load_view('pickANumber')
-  self.v_main.present('fullscreen')
+    self.v_main=ui.load_view('pickANumber')
+    self.v_main.present('fullscreen')
 
-def rank(self, sender):
+  def rank(self, sender):
 
-  def closeme(sender):
-    self.v_results.close()
+    def closeme(sender):
+      self.v_results.close()
 
-  minNum = int(self.v_main['minNum'].text)
-  maxNum = int(self.v_main['maxNum'].text)
-  randPick = random.randint(minNum,maxNum)
-  valDict={}
+    minNum = int(self.v_main['minNum'].text)
+    maxNum = int(self.v_main['maxNum'].text)
+    randPick = random.randint(minNum,maxNum)
+    valDict={}
 
-  for i in range(6):
-    iName='name_'+str(i)
-    iNum='num_'+str(i)
-    if self.v_main[iNum].text:
-      valDict[self.v_main[iName].text]=int(self.v_main[iNum].text)
+    for i in range(6):
+      iName='name_'+str(i)
+      iNum='num_'+str(i)
+      if self.v_main[iNum].text:
+        valDict[self.v_main[iName].text]=int(self.v_main[iNum].text)
 
-  diffNamesDict={}
-  for nameVal in valDict:
-    valDiff = abs(valDict[nameVal]-randPick)
-    diffNamesDict[nameVal]=valDiff
+    diffNamesDict={}
+    for nameVal in valDict:
+      valDiff = abs(valDict[nameVal]-randPick)
+      diffNamesDict[nameVal]=valDiff
 
-  resultList = [];
+    resultList = [];
 
-  for key,value in sorted(diffNamesDict.items(),key=lambda x:x[1]):
-    nameResult = f"{key}: \u0394{value}"
-    resultList.append(nameResult)
-  
-  self.v_results = ui.load_view('pANpopout')
-  self.v_results.present('popover')
-  resultViewBox = self.v_results['results']
-  resultViewBox.data_source=resultList
+    for key,value in sorted(diffNamesDict.items(),key=lambda x:x[1]):
+      nameResult = f"{key}: \u0394{value}"
+      resultList.append(nameResult)
 
+    self.v_results = ui.load_view('pANpopout')
+    self.v_results.present('popover')
+    resultViewBox = self.v_results['results']
+    resultViewBox.data_source=resultList
+
+PickANum()
